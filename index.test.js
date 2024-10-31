6c35fbfb1eb7a92efd7d52d7e5db640c8d0df885
JRSkates <skatesproduction@gmail.com>
2024-10-31 13:58:11 +0000

    full tests written

--- index.test.js
+++ index.test.js
@@ -14,7 +14,7 @@ describe('Social Sequelzie Test', () => {
     // Write your tests here
     
     test("Constructs User Model", async () => {
-        const user = new User({
+        const user = await new User({
             username: "jsj179",
             email: "jsj179@gmail.com"
         });
@@ -22,7 +22,7 @@ describe('Social Sequelzie Test', () => {
     })
 
     it("Constructs Profile Model", async () => {
-        const profile = new Profile({
+        const profile = await new Profile({
             bio: "I'm a software engineer",
             profilePicture: "profilePicture",
             birthday: "25/07/1996"
@@ -31,7 +31,7 @@ describe('Social Sequelzie Test', () => {
     })
 
     it("Constructs Post Model", async () => {
-        const post = new Post({
+        const post = await new Post({
             title: "First Post",
             body: "This is my first post",
             createdAt: "2022-01-01 12:00:00"
@@ -40,7 +40,7 @@ describe('Social Sequelzie Test', () => {
     })
 
     it("Constructs Comment Model", async () => {
-        const post = new Comment({
+        const post = await new Comment({
             body: "This is my first comment",
             createdAt: "2022-01-01 12:00:00"
         });
@@ -48,10 +48,75 @@ describe('Social Sequelzie Test', () => {
     })
 
     it("Constructs Like Model", async () => {
-        const like = new Like({
+        const like = await new Like({
             reactionType: "like",
             createdAt: "2022-01-01 12:00:00"
         });
         expect(like.reactionType).toBe('like');
+    })
+
+    it("User-Profile association" , async () => {
+        const user = await User.create({
+            username: "jsj179",
+            email: "jsj179@gmail.com"
+        });
+        const profile = await Profile.create({
+            bio: "I'm a software engineer",
+            profilePicture: "profilePicture",
+            birthday: "25/07/1996"
+        });
+
+        await user.setProfile(profile);
+        const userProfile = await user.getProfile()
+
+        expect(userProfile.bio).toBe("I'm a software engineer");
+    })
+
+    it("User-Post association" , async () => {
+        const user = await User.create({
+            username: "jsj179",
+            email: "jsj179@gmail.com"
+        });
+        const post = await Post.create({
+            title: "First Post",
+            body: "This is my first post",
+            createdAt: "2022-01-01 12:00:00"
+        });
+
+        await user.addPost(post);
+        const userPosts = await user.getPosts()
+        expect(userPosts.length).toBe(1);
+    })
+
+    it("Post-Comment association" , async () => {
+        const post = await Post.create({
+            title: "First Post",
+            body: "This is my first post",
+            createdAt: "2022-01-01 12:00:00"
+        });
+
+        const comment = await Comment.create({
+            body: "This is my first comment",
+            createdAt: "2022-01-01 12:00:00"
+        })
+
+        await post.addComment(comment);
+        const postComments = await post.getComments()
+        expect(postComments.length).toBe(1);
+    })
+
+    it("User-Likes association", async () => {
+        const user = await User.create({
+            username: "jsj179",
+            email: "jsj179@gmail.com"
+        });
+        const like = await Like.create({
+            reactionType: "like",
+            createdAt: "2022-01-01 12:00:00"
+        });
+
+        await user.addLike(like);
+        const userLikes = await user.getLikes()
+        expect(userLikes.length).toBe(1);
     })
 })
